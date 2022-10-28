34dbd5d7cce150754b56c8d651cbfce8f9486b48
ginkgoMZD <mzd@ginkgostreet.com>
2022-10-28 14:46:13 -0400

    update decomposer from upstream;

--- decomposer.mk
+++ decomposer.mk
@@ -22,6 +22,10 @@ STACK_ID := $(shell echo "${STACK}" | tr a-z A-Z)
 include ${STACK_NAME}.stack
 endif
 
+ifdef STACK_SERVICES
+STACK_SERVICES := $(shell basename -as .yml ${STACK_SERVICES})
+endif
+
 # do not change default goal by this include:
 .DEFAULT_GOAL_CACHED := ${.DEFAULT_GOAL}
 
@@ -47,12 +51,11 @@ rm-file = $(if $(shell (test -L $1 || test -e $1) && echo 'TRUE'), \
 define stack-env-includes
 /dev/null \
 $(foreach svc,${STACK_SERVICES} ${TASK},$(call if-file-in,docker,${svc}.env)) \
-$(foreach stk,${STACK_SERVICES} ${STACK_ID} ${TASK},$(call if-file-in,.,${stk}.stack.env)) \
+$(foreach stk,${STACK_NAME} ${TASK},$(call if-file-in,.,${stk}.stack.env)) \
 ${ENV_INCLUDES}
 endef
 
 %.stack.env: ${stack-env-includes}
-	$(info using $(filter-out /dev/null,$^))
 	@cat $^ >$@
 
 .INTERMEDIATE: ${STACK_NAME}.stack.env # enable auto-clean-up of generated files
@@ -64,12 +67,13 @@ endef
 # include YAML files named for the STACK in supported locations
 # include service/task definitions in docker/
 define stack-config-includes
-$(foreach type,network volume config conf,$(call if-file-in,${type},${STACK_NAME}.yml))\
-$(foreach svc,${STACK_SERVICES},$(call if-file-in,docker,${svc}.yml))
+$(strip \
+	$(foreach type,network volume,$(call if-file-in,${type},${STACK_NAME}.yml))\
+	$(foreach svc,${STACK_SERVICES},$(call if-file-in,docker,${svc}.yml))
+)
 endef
 
 %-compose.yml: ${stack-config-includes}
-	$(info using $^)
 	@docker-compose --project-directory=. $(foreach f,$^,-f $f) config > $@ 2>/dev/null
 
 .INTERMEDIATE: ${STACK_NAME}-compose.yml # enable auto-clean-up of generated files
@@ -77,30 +81,35 @@ endef
 docker-compose.yml: ${STACK_NAME}-compose.yml
 	@cp $< $@
 
+define REPORT_STACK
+$(info STACK: ${STACK})\
+$(info ENV SOURCES: $(filter-out /dev/null,${stack-env-includes}))\
+$(info SERVICES: ${STACK_SERVICES})\
+$(info COMPOSER SOURCES: ${stack-config-includes})\
+$(info )
+endef
+
 ifdef DEBUG
-$(info STACK::${STACK})
-$(info stack-config-includes::$(strip ${stack-config-includes}))
-$(info stack-env-includes::$(strip ${stack-env-includes}))
+$(call REPORT_STACK)
 endif
 
 # # #
 # Commands
 # # #
 
-activate:
+activate: | deactivate
 ifeq (${STACK},NULL)
-	$(eval export STACK=${INACTIVE})
+	$(eval STACK=${INACTIVE})
 endif
-	@$(MAKE) --quiet .env docker-compose.yml 
 	@echo "STACK=${STACK}" > .active
-	$(info STACK:${STACK})
-	$(info SERVICES:${STACK_SERVICES})
+	@$(MAKE) --quiet .env docker-compose.yml 
+	$(REPORT_STACK)
 
 deactivate:
 	$(foreach f,.env docker-compose.yml ${STACK_NAME}.stack.env ${STACK_NAME}-compose.yml,\
 		$(call rm-file,$f))
 ifneq (${STACK},NULL)
-	echo "INACTIVE=${STACK}" > .active
+	@echo "INACTIVE=${STACK}" > .active
 endif
 
 # # #
